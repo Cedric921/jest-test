36800b6305e519700eb52b080271410d4b3758e3
cedric karungu <ckarungu921@gmail.com>
2022-07-12 16:54:21 +0200

    feat: add test to array length

---
+++ 03-setup/setup.spec.js
@@ -0,0 +1,13 @@
+const animals = ['zebra', 'elephant', 'bear', 'tiger'];
+
+describe('Animals array', () => {
+    it('Should have initial lenght of 4', () => {
+        expect(animals.length).toBe(4);
+    });
+    
+	it('should add animals to end of array', () => {
+		animals.push('aligator');
+		expect(animals[animals.length - 1]).toBe('aligator');
+	});
+
+});
