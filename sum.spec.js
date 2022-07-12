0a90fd8e8681cbca966a168b0b53de380ee41f22
cedric karungu <ckarungu921@gmail.com>
2022-07-12 15:51:20 +0200

    fix: fix merge missing

--- sum.spec.js
+++ sum.spec.js
@@ -1,7 +1,26 @@
+// const { describe, it } = require('jest-circus');
 const sum = require('./sum');
 
+describe('Example ', () => {
+	it('should add 1 + 2 to equal 3', () => {
+		const result = sum(1, 2);
+		expect(result).toBe(3);
+	});
 
-test('should add 1 + 2 to equal 3', () => {
-    const result = sum(1,2);
-    expect(result).toBe(3);
-})
+	it('Object assignement', () => {
+		const obj = {};
+		expect(obj).toEqual({});
+	});
+});
+
+describe('Test true or false', () => {
+	it('Is not null', () => {
+		const number = 12;
+		expect(number).toBeTruthy();
+	});
+
+	it('Is null', () => {
+		const otherNumber = undefined;
+		expect(otherNumber).toBeFalsy();
+	});
+});
