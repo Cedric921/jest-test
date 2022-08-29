d3b302ca1d08c2da401137f66e4b530a385df7cb
cedric karungu <ckarungu921@gmail.com>
2022-08-29 09:34:19 +0200

    refact: refact test

--- 03-setup/setup.spec.js
+++ 03-setup/setup.spec.js
@@ -1,10 +1,12 @@
 const animals = ['zebra', 'elephant', 'bear', 'tiger'];
 
+
+
 describe('Animals array', () => {
     it('Should have initial lenght of 4', () => {
         expect(animals.length).toBe(4);
     });
-    
+
 	it('should add animals to end of array', () => {
 		animals.push('aligator');
 		expect(animals[animals.length - 1]).toBe('aligator');
